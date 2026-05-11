a7e84bba508610bcbcac9703fda372c4a0028e1f
cklgy88 <cklgy88@gmail.com>
2026-05-11 09:28:29 +0800

    first commit

---
+++ src/main/java/com/example/Family_life_backend/FamilyLifeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Family_life_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyLifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyLifeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Family_life_backend/FamilyLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Family_life_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyLifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
