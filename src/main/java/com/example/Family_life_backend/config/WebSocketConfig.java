30df217a62d5791128c068573d812879e96381dd
David <davids710119@gmail.com>
2026-06-17 10:36:31 +0800

    圖片顯示，不用管

--- src/main/java/com/example/Family_life_backend/config/WebSocketConfig.java
+++ src/main/java/com/example/Family_life_backend/config/WebSocketConfig.java
@@ -13,14 +13,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-<<<<<<< HEAD
-=======
-
-		registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
-
-//                .withSockJS(); // ⭐ 很重要
-
->>>>>>> origin/feature-backend
 //		registry.addEndpoint("/ws").setAllowedOriginPatterns("http://localhost:4200", "http://127.0.0.1:4200");
 		registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
 //                .withSockJS(); // ⭐ 很重要
