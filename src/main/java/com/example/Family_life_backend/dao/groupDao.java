6e152bc7261faa70bc8ac181ff40d6cf4d809e0d
a047354166 <a047354166@gmail.com>
2026-06-17 13:58:19 +0800

    修改bug

--- src/main/java/com/example/Family_life_backend/dao/groupDao.java
+++ src/main/java/com/example/Family_life_backend/dao/groupDao.java
@@ -71,7 +71,7 @@ public interface groupDao extends JpaRepository<group, Long> {
 
 	@Modifying
 	@Transactional
-	@Query(value = "update `groups` set group_name = :groupName, avatar = :Avatar, created_by = :createdBy where group_id = :groupId", nativeQuery = true)
+	@Query(value = "update `groups` set group_name = :groupName, avatar = :Avatar  where group_id = :groupId", nativeQuery = true)
 	void updateGroup(@Param("groupName") String groupName, @Param("Avatar") String Avatar,
 			@Param("groupId") Long groupId);
 
